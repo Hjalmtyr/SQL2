02f1b6b288d01fd2374749ddb6060499a7f44f23
Hjalmtyr <hh@hi.is>
2024-11-04 08:56:50 +0000

    Fyrir námskeið í nóv. 2024

--- sumarhus.sql
+++ sumarhus.sql
@@ -1,18 +1,18 @@
 ---------------------------------------------------------------------------
---  SkipanaskrÃ¡ til aÃ° bÃºa til tÃ¶flur fyrir EndurmenntunarnÃ¡mskeiÃ°
---      Ã­ SQL
+--  Skipanaskrá til ağ búa til töflur fyrir Endurmenntunarnámskeiğ
+--      í SQL
 --
---  HjÃ¡lmtÃ½r Hafsteinsson, aprÃ­l 2023.
+--  Hjálmtır Hafsteinsson, október 2024.
 ---------------------------------------------------------------------------
 
--- Henda Ãºt gÃ¶mlum ÃºtgÃ¡fum af tÃ¶flum
+-- Henda út gömlum útgáfum af töflum
 
 drop table if exists felagar;
 drop table if exists leigur;
 drop table if exists sumarhus;
 
 
--- BÃºa til tÃ¶flurnar
+-- Búa til töflurnar
 
 create table felagar(
         fnr integer,
@@ -34,7 +34,7 @@ create table leigur(
         fj_daga integer);
 
 
--- Setja inn sÃ½nisgÃ¶gn
+-- Setja inn sınisgögn
 
 insert into felagar (fnr, nafn, postnr, stig, inng_ar)
         values (48, 'Gunnar', 107, 450, 2017);
@@ -53,7 +53,7 @@ insert into felagar (fnr, nafn, postnr, stig, inng_ar)
 insert into felagar (fnr, nafn, postnr, stig, inng_ar)
         values (24, 'Gunnar', 107, 330, 1999);
 insert into felagar (fnr, nafn, postnr, stig, inng_ar)
-        values (85, 'Anna', 200, 110, 2014);
+        values (85, 'Anna', 200, 110, 2021);
 insert into felagar (fnr, nafn, postnr, stig, inng_ar)
         values (45, 'Birna', 112, 215, 2015);
 
@@ -62,7 +62,7 @@ insert into sumarhus (husnr, stadur, fermetrar, fj_ruma)
 insert into sumarhus (husnr, stadur, fermetrar, fj_ruma)
         values (1002, 'Laugarvatn 2', 65, 6);
 insert into sumarhus (husnr, stadur, fermetrar, fj_ruma)
-        values (1004, 'Reykir', 42, 5);
+        values (1004, 'Reykir', 55, 5);
 insert into sumarhus (husnr, stadur, fermetrar, fj_ruma)
         values (1005, 'Husafell A1', 50, 6);
 insert into sumarhus (husnr, stadur, fermetrar, fj_ruma)
@@ -71,22 +71,22 @@ insert into sumarhus (husnr, stadur, fermetrar, fj_ruma)
         values (1012, 'Grimsnes 45', 120, 10);
 
 insert into leigur (fnr, husnr, dags, fj_daga)
-        values (48, 1001, '2021-07-08', 7);
+        values (48, 1001, '2024-07-08', 7);
 insert into leigur (fnr, husnr, dags, fj_daga)
-        values (48, 1005, '2020-07-16', 7);
+        values (48, 1005, '2023-07-16', 7);
 insert into leigur (fnr, husnr, dags, fj_daga)
-        values (12, 1005, '2021-02-13', 3);
+        values (12, 1005, '2024-02-13', 3);
 insert into leigur (fnr, husnr, dags, fj_daga)
-        values (32, 1004, '2019-06-20', 7);
+        values (32, 1004, '2022-06-20', 7);
 insert into leigur (fnr, husnr, dags, fj_daga)
-        values (32, 1004, '2021-08-05', 7);
+        values (32, 1004, '2024-08-05', 7);
 insert into leigur (fnr, husnr, dags, fj_daga)
-        values (85, 1002, '2021-07-01', 7);
+        values (85, 1002, '2024-07-01', 7);
 insert into leigur (fnr, husnr, dags, fj_daga)
-        values (85, 1001, '2020-11-05', 3);
+        values (85, 1001, '2023-11-05', 3);
 insert into leigur (fnr, husnr, dags, fj_daga)
-        values (71, 1001, '2021-07-08', 7);
+        values (71, 1001, '2024-08-10', 7);
 insert into leigur (fnr, husnr, dags, fj_daga)
-        values (64, 1012, '2020-10-14', 3);
+        values (64, 1012, '2023-10-14', 3);
 
 
